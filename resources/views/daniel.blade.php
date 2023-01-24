6aeda5d9d27ccc21aebf7671a57dbbd8a04304e8
zStrikke <zstrikke@gmail.com>
2023-01-25 00:44:46 +0100

    solucionado el hotfix del MU

--- resources/views/daniel.blade.php
+++ resources/views/daniel.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-ES MU BUENA GENTE
+ES MUY BUENA GENTE
                                 </div>
                             </div>
                         </div>
